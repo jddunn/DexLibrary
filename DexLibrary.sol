720462aed7e1c665bfe2176b847e37d9ab1f3db2
johnny <johnnyfived@protonmail.com>
2022-06-08 21:56:00 -0700

    add removeLiquidity interface funcs

--- DexLibrary.sol
+++ DexLibrary.sol
@@ -64,6 +64,25 @@ interface IDEXRouter {
             uint256 liquidity
         );
 
+    function removeLiquidityETHSupportingFeeOnTransferTokens(
+        address token,
+        uint liquidity,
+        uint amountTokenMin,
+        uint amountETHMin,
+        address to,
+        uint deadline
+    ) external returns (uint amountETH);
+    
+    function removeLiquidityETHWithPermitSupportingFeeOnTransferTokens(
+        address token,
+        uint liquidity,
+        uint amountTokenMin,
+        uint amountETHMin,
+        address to,
+        uint deadline,
+        bool approveMax, uint8 v, bytes32 r, bytes32 s
+    ) external returns (uint amountETH);
+
     function swapExactTokensForTokensSupportingFeeOnTransferTokens(
         uint256 amountIn,
         uint256 amountOutMin,
